f2acfd83836487e3a7a26798fd9c034f453ad7d4
Dumopolis <den_980123@mail.ru>
2022-11-29 09:08:57 +0300

    bug fixed

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
-import {scroll, handleButton, useAnimate} from './scrollify'
-import {timer} from './timer'
-import {comments} from './commentSlick'
-import {event} from './event'
-import {form} from './form'
+import { scroll, handleButton, useAnimate } from './scrollify'
+import { timer } from './timer'
+import { comments } from './commentSlick'
+import { event } from './event'
+import { form } from './form'
+import { videoLoader } from './videoLoader'
 
-$(window).on('load', function(){
+$(window).on('load', function () {
   event()
   comments()
   timer()
@@ -13,4 +14,6 @@ $(window).on('load', function(){
   handleButton();
   useAnimate();
   form();
+  videoLoader();
 })
+$(window).resize(videoLoader) 
